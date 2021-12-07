8656e964d01322c65ba1f3166842a2a3d5d0b3d3
Martin Kapun <capoony@gmail.com>
2021-12-07 21:40:24 +0100

    update

---
+++ shell/main.sh
@@ -0,0 +1,13 @@
+###
+
+## store log files
+
+mkdir /media/inter/mkapun/projects/Trochilus_ddRAD/log
+
+## restructure and copy data from Luise's folder
+
+sh /media/inter/mkapun/projects/Trochilus_ddRAD/shell/restructure.sh
+
+## trim raw reads
+
+sh /media/inter/mkapun/projects/Trochilus_ddRAD/shell/trimming.sh
